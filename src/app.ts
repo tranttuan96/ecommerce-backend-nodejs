c067ef406555618a26b9205f3249f47187a91adb
tranttuan96 <tranttuan96@gmail.com>
2024-03-29 12:02:56 +0700

    setup morgan, helmet, compression

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
+import compression from "compression";
 import express, { Express } from "express";
+import helmet from "helmet";
+import morgan from "morgan";
 
 const app: Express = express();
 
 // init middlewares
+app.use(
+  morgan(
+    "[:date[iso]] :remote-addr HTTP/:http-version :method :url HTTP-Code: :status Size: :res[content-length] bytes - Response-time: :response-time ms"
+  )
+);
+app.use(helmet())
+app.use(compression())
 
 // init db
 
 // init routes
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Welcome!",
+  });
+});
 
 // handling errors
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const server = app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
